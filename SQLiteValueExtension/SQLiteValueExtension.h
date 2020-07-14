b07388c1ef645297432fbb815ca7c5c12dfc9887
pjx <pujx@jingbo360.com>
2020-07-14 09:38:20 +0800

    Initial Commit

---
+++ SQLiteValueExtension/SQLiteValueExtension.h
@@ -0,0 +1,19 @@
+//
+//  SQLiteValueExtension.h
+//  SQLiteValueExtension
+//
+//  Created by jiaxin on 2020/7/14.
+//  Copyright © 2020 jiaxin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SQLiteValueExtension.
+FOUNDATION_EXPORT double SQLiteValueExtensionVersionNumber;
+
+//! Project version string for SQLiteValueExtension.
+FOUNDATION_EXPORT const unsigned char SQLiteValueExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLiteValueExtension/PublicHeader.h>
+
+
